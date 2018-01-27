9426d677e67d6e8248c078168b879ba952e53ce0
cunxi <cunxi1992@gmail.com>
2018-01-27 23:40:51 +0800

    解决bug，主叫号码计算错误

--- Task3.py
+++ Task3.py
@@ -59,23 +59,22 @@ called_phone_list = []
 
 for call in calls:
     # 找出被班加罗尔地区的固定电话所拨打的所有电话
-    # find()函数返回包含指定子字符串开始的索引值，若不存在则返回-1
-    if call[0].find('(080)') == 0:
-        if call[1].find('(') == 0:
+    # startswith()方法用于检查字符串是否是以指定子字符串开头，如果是则返回 True，否则返回 False
+    if call[0].startswith('(080)'):
+        # 计算被班加罗尔地区的固定电话所拨打的电话总数
+        total_phone_numbers += 1
+        if call[1].startswith('('):
             # 若被叫号码是以'('开头，则获取以')'结束的索引值+1
             end_index = call[1].find(')') + 1
             # 调用函数，将被拨打电话的代号加入到指定列表中
             called_phone_symbol(call[1][0:end_index],called_phone_list)
-            # 计算被班加罗尔地区的固定电话所拨打的电话总数
-            total_phone_numbers += 1
             # 计算由班加罗尔地区的固定电话拨打到班加罗尔地区的电话总数
-            if call[1].find('080') == 0:
+            if call[1].find('080') == 1:
                 ban_phone_numbers += 1
         # 判断被拨打号码的前缀是否为7、8、9，若是，则将其添加到指定列表中
         elif call[1][0] in ['7','8','9']:
             called_phone_symbol(call[1][0:4],called_phone_list)
-            # 计算被班加罗尔地区的固定电话所拨打的电话总数
-            total_phone_numbers += 1
+
 
 # 对列表元素进行永久性排序
 called_phone_list.sort()
@@ -89,12 +88,8 @@ for phone in called_phone_list:
 
 # ****  第二部分  ****
 # 计算由班加罗尔固话打往班加罗尔的电话所占比例是多少
-percent = ban_phone_numbers / total_phone_numbers * 100
+percent = float(ban_phone_numbers) / float(total_phone_numbers) * 100
 print("{:.2f} percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.".format(percent))
-
-
-
-
 
 
 
